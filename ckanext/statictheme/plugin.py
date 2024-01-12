5a4674ce6074ae42aa66b8b7d65396e98e08fac6
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-01-12 16:28:13 +0100

    home repo count

--- ckanext/statictheme/plugin.py
+++ ckanext/statictheme/plugin.py
@@ -1,12 +1,29 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-''' To display repository's dataset on the home page'''
-def repositories_present(data_dict = None):
 
-    org =  toolkit.get_action('organization_list')(
-       data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+def repositories_present():
+    org = toolkit.get_action('organization_list')(
+        data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+
     return org
+
+
+def repositories_count():
+    list_org = toolkit.get_action('organization_list')(
+        data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+
+    count_to_display = len(list_org)
+
+    return count_to_display
+
+
+def dataset_count():
+    package_list = toolkit.get_action('package_list')({},{})
+
+    count_to_display = len(package_list)
+
+    return count_to_display
 
 
 class StaticthemePlugin(plugins.SingletonPlugin):
@@ -22,6 +39,7 @@ class StaticthemePlugin(plugins.SingletonPlugin):
         toolkit.add_resource('fanstatic',
                              'statictheme')
 
-
     def get_helpers(self):
-        return {'repositories_present': repositories_present}
+        return {'repositories_present': repositories_present,
+                'repositories_count': repositories_count,
+                'dataset_count':  dataset_count,}
