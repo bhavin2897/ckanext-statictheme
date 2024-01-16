54d7a727958fc540a7462818df0bfa1485d4a783
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-01-16 10:20:30 +0100

    repo count and dataset count from same API

--- ckanext/statictheme/plugin.py
+++ ckanext/statictheme/plugin.py
@@ -9,28 +9,35 @@ def repositories_present():
     return org
 
 
-def repositories_count():
-    """Number of repositories in CKAN organizations"""
+def repositories_dataset_count():
+    """Number of repositories in CKAN organizations &
+    Number of datasets in the repositories list. """
+    each_repo_count = []
     list_org = toolkit.get_action('organization_list')(
         data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
-    count_to_display = len(list_org)
+    count_to_display_repo = len(list_org)
 
-    return count_to_display
-
-
-def dataset_count():
-    """ Number of datasets from each of the repository"""
-    each_repo_count = []
-    repository_list = toolkit.get_action('organization_list')(
-        data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
-
-    for package_count in repository_list:
+    for package_count in list_org:
         each_repo_count.append(package_count['package_count'])
 
-    count_to_display = sum(each_repo_count)
-    # count_to_display = 1000
+    count_to_display_dataset = sum(each_repo_count)
 
-    return count_to_display
+    return count_to_display_repo,count_to_display_dataset
+
+
+# def dataset_count():
+#     """ Number of datasets from each of the repository"""
+#     each_repo_count = []
+#     repository_list = toolkit.get_action('organization_list')(
+#         data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+#
+#     for package_count in repository_list:
+#         each_repo_count.append(package_count['package_count'])
+#
+#     count_to_display = sum(each_repo_count)
+#     # count_to_display = 1000
+#
+#     return count_to_display
 
 
 class StaticthemePlugin(plugins.SingletonPlugin):
@@ -48,5 +55,5 @@ class StaticthemePlugin(plugins.SingletonPlugin):
 
     def get_helpers(self):
         return {'repositories_present': repositories_present,
-                'repositories_count': repositories_count,
-                'dataset_count':  dataset_count,}
+                'repositories_dataset_count': repositories_dataset_count, }
+                # 'dataset_count':  dataset_count,}
