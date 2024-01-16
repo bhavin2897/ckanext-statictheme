0bf7a0b91a6b932f32556e999ff9dacb5cd39b2c
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-01-16 11:16:06 +0100

    refactoring

--- ckanext/statictheme/plugin.py
+++ ckanext/statictheme/plugin.py
@@ -2,19 +2,22 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-def repositories_present():
-    """ Datasets present in the repositories list. """
-    org = toolkit.get_action('organization_list')(
-        data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
-    return org
+# def repositories_present():
+#     """ Datasets present in the repositories list. """
+#     org = toolkit.get_action('organization_list')(
+#         data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+#     return org
 
 
-def repositories_dataset_count():
+def repositories_dataset_present_count():
     """Number of repositories in CKAN organizations &
     Number of datasets in the repositories list. """
     each_repo_count = []
+
     list_org = toolkit.get_action('organization_list')(
         data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+
+    org = list_org
     count_to_display_repo = len(list_org)
 
     for package_count in list_org:
@@ -22,7 +25,7 @@ def repositories_dataset_count():
 
     count_to_display_dataset = sum(each_repo_count)
 
-    return count_to_display_repo,count_to_display_dataset
+    return org,count_to_display_repo,count_to_display_dataset
 
 
 class StaticthemePlugin(plugins.SingletonPlugin):
@@ -39,6 +42,6 @@ class StaticthemePlugin(plugins.SingletonPlugin):
                              'statictheme')
 
     def get_helpers(self):
-        return {'repositories_present': repositories_present,
-                'repositories_dataset_count': repositories_dataset_count, }
+        return { #'repositories_present': repositories_present,
+                'repositories_dataset_present_count': repositories_dataset_present_count, }
                 # 'dataset_count':  dataset_count,}
