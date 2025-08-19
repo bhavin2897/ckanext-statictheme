91b82307a69d196b2685963f9ff9f567472220b9
Bhavin Katabathuni <bhavin2897@gmail.com>
2025-08-19 09:27:28 +0200

    style: display latest time of the latest dataset

--- ckanext/statictheme/helpers.py
+++ ckanext/statictheme/helpers.py
@@ -10,14 +10,73 @@ import logging
 log = logging.getLogger(__name__)
 
 
+# def repositories_dataset_present_count():
+#     """Number of repositories in CKAN organizations &
+#     Number of datasets in the repositories list. """
+#     each_repo_count = []
+#     organization_details = []
+#
+#     list_org = toolkit.get_action('organization_list')(
+#         data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+#
+#     count_to_display_repo = len(list_org)
+#
+#     for org in list_org:
+#         try:
+#             get_org = toolkit.get_action('organization_show')(
+#                 data_dict={
+#                     'id': org['id'],
+#                     'include_datasets': True,
+#                     'include_dataset_count': True
+#                 }
+#             )
+#
+#             # Filter only datasets of type 'dataset'
+#             filtered_datasets = [
+#                 ds for ds in get_org.get('packages', [])
+#                 if ds.get('type') == 'dataset'
+#             ]
+#
+#             # Sort by metadata_modified descending and take 5 latest
+#             latest_datasets = sorted(
+#                 filtered_datasets,
+#                 key=lambda x: x.get('metadata_modified', ''),
+#                 reverse=True
+#             )[:5]
+#
+#             # Overwrite 'packages' key with the reduced list
+#             get_org['packages'] = latest_datasets
+#
+#             organization_details.append(get_org)
+#
+#         except toolkit.ObjectNotFound:
+#             log.error(f"Organization '{org}' not found.")
+#
+#     for package_count in list_org:
+#         each_repo_count.append(package_count['package_count'])
+#
+#     count_to_display_dataset = sum(each_repo_count)
+#
+#
+#     return count_to_display_repo, count_to_display_dataset, organization_details,
+
+
 def repositories_dataset_present_count():
-    """Number of repositories in CKAN organizations &
-    Number of datasets in the repositories list. """
+    """
+    Number of repositories in CKAN organizations &
+    Number of datasets in the repositories list.
+    Also returns organization details and a mapping of datasets to metadata_modified.
+    """
     each_repo_count = []
     organization_details = []
 
+    # NEW: mappings of dataset -> metadata_modified
+    datasets_modified = {}              # flat map: {dataset_id: metadata_modified}
+    datasets_modified_by_org = {}       # nested: {org_id: {dataset_id: metadata_modified}}
+
     list_org = toolkit.get_action('organization_list')(
-        data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True})
+        data_dict={'type': 'repository', 'sort': 'package_count desc', 'all_fields': True}
+    )
 
     count_to_display_repo = len(list_org)
 
@@ -46,19 +105,31 @@ def repositories_dataset_present_count():
 
             # Overwrite 'packages' key with the reduced list
             get_org['packages'] = latest_datasets
-
             organization_details.append(get_org)
+
+            # Build the mappings
+            datasets_modified_by_org[get_org['id']] = {
+                ds['id']: ds.get('metadata_modified') for ds in latest_datasets
+            }
+            for ds in latest_datasets:
+                datasets_modified[ds['id']] = ds.get('metadata_modified')
 
         except toolkit.ObjectNotFound:
             log.error(f"Organization '{org}' not found.")
 
     for package_count in list_org:
-        each_repo_count.append(package_count['package_count'])
+        each_repo_count.append(package_count.get('package_count', 0))
 
     count_to_display_dataset = sum(each_repo_count)
 
+    return (
+        count_to_display_repo,
+        count_to_display_dataset,
+        organization_details,
+        datasets_modified,          # flat
+        datasets_modified_by_org    # grouped
+    )
 
-    return count_to_display_repo, count_to_display_dataset, organization_details
 
 
 
